b4b32843471ca20b79eb4b95c7aa244ac040b0d6
TalChenBE <talchenben1234@gmail.com>
2023-01-02 17:47:03 +0200

    update login,signup, navbar

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
 # Car Managment:
-
-my installation:
-
-```
-npm i react-router-dom
-npm i react-router
-```
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import "./Navbar.css";
 import male from "../../utils/icons/male.svg";
 import female from "../../utils/icons/female.svg";
 import car from "../../utils/icons/car.svg";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ fullName = "FirstName LastName", gender = "female" }) => {
   return (
@@ -17,6 +18,10 @@ const Navbar = ({ fullName = "FirstName LastName", gender = "female" }) => {
 
       <h6>{fullName}</h6>
 
+      <a className="navbar-title-ContectUs" href="../ContectUs">
+        Contect Us
+      </a>
+      <div className="navbar-divider"></div>
       <h4 className="navbar-title">Car Menagment</h4>
       <img src={car} alt="car-icon" className="navbar-img-icon"></img>
     </div>
