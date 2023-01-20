3003ddce875cb13228f973e425b760f258816bf9
MoshPe <fghghf98@gmail.com>
2023-01-20 13:28:55 +0200

    complete remember me token based

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 const BASE_URL = "https://car-management-back-end.vercel.app/api";
-const TEST_URL = 'http://localhost:4000/api';
+// const TEST_URL = 'http://localhost:4000/api';
 
 export default axios.create({
-  baseURL: TEST_URL,
+  baseURL: BASE_URL,
 });
 
 export const axiosPrivate = axios.create({
-  baseURL: TEST_URL,
+  baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
