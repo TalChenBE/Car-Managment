7e1e060fcfed78dca84bc6091dfdd87a72420a8a
TalChenBE <talchenben1234@gmail.com>
2023-01-23 00:56:00 +0200

    remove general recapcha

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthProvider";
-import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <GoogleReCaptchaProvider reCaptchaKey={process.env.REACT_APP_SITE_KEY}>
-    <React.StrictMode>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </React.StrictMode>
-  </GoogleReCaptchaProvider>
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 );
